eb2307f7034d5f417bedfa5ebd526fdcb48c9910
Mateus Rocha <58711427+matrocheetos@users.noreply.github.com>
2023-05-11 22:26:42 -0300

    ppi trabalho9 ex2 ex3 ex4 ex5 ex6

---
+++ programacao-para-internet/trabalho9/ex6/busca-endereco.php
@@ -0,0 +1,62 @@
+<?php
+
+function mysqlConnect()
+{
+  $db_host = "host";
+  $db_username = "user";
+  $db_password = "pass";
+  $db_name = "db";
+
+  // dsn é apenas um acrônimo de database source name
+  $dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8mb4";
+
+  $options = [
+    PDO::ATTR_EMULATE_PREPARES => false, // desativa a execução emulada de prepared statements
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // ativa o modo de erros para lançar exceções    
+    PDO::ATTR_PERSISTENT    => true, // ativa o uso de conexões persistentes para maior eficiência
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // altera o modo padrão do método fetch para FETCH_ASSOC
+  ];
+
+  try {
+    $pdo = new PDO($dsn, $db_username, $db_password, $options);
+    return $pdo;
+  } 
+  catch (Exception $e) {
+    //error_log($e->getMessage(), 3, 'log.php');
+    exit('Ocorreu uma falha na conexão com o BD: ' . $e->getMessage());
+  }
+}
+
+$pdo = mysqlConnect();
+
+class Endereco
+{
+  public $rua;
+  public $bairro;
+  public $cidade;
+
+  function __construct($rua, $bairro, $cidade)
+  {
+    $this->rua = $rua;
+    $this->bairro = $bairro;
+    $this->cidade = $cidade;
+  }
+}
+
+$cep = $_GET['cep'] ?? '';
+
+$sql = <<<SQL
+  SELECT rua, bairro, cidade
+  FROM endereco
+  WHERE cep = ?
+  SQL;
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$cep]);
+
+while ($row = $stmt->fetch()) {
+  $endereco = new Endereco($row['rua'], $row['bairro'], $row['cidade']);
+}
+
+header('Content-type: application/json');
+echo json_encode($endereco);
